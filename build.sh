259130cf7308a8f28dd17a6dd904200fcf6fd640
Romain DUPONT <3.romain.dupont@gmail.com>
2018-03-20 09:18:28 +0100

    add gitignore

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-REGISTRY="kuajstry.ddns.net:5000/"
 IMAGE_NAME="grenouille-discord"
-IMAGE="$REGISTRY$IMAGE_NAME"
+IMAGE_VERSION="1"
 TOKEN="$(cat token.txt)"
 
-docker build -t $IMAGE .
-#docker push $IMAGE
+docker build -t $IMAGE:$IMAGE_VERSION .
